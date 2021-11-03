ac6566357b6f2a9f1c3faa17f0e5d38d6e26e95b
Bruno Rocha <bruno.rocha.dev@outlook.com>
2021-11-03 17:34:33 -0300

    Config dotenv

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/spottasks', {
+mongoose.connect(`mongodb+srv://brrunorocha:${process.env.DBKEY}@cluster0.lnhrh.mongodb.net/spottasks?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
